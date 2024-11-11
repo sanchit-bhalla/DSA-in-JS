000c2e54030a8c38a5c0ccc5c4502cabcff1d7f0
Sanchit Bhalla <sanchitbhalla91@gmail.com>
2024-11-11 12:56:12 +0530

    DFS Recursive

--- Graph/graph.js
+++ Graph/graph.js
@@ -29,9 +29,30 @@ class Graph {
     }
     delete this.adjacencyList[vertex];
   }
+
+  // DFS Recursively
+  depthFirstRecursive(start) {
+    const result = [];
+    const visited = {};
+    const adjacencyList = this.adjacencyList; // Inside below IIFE `this` will be undefined
+
+    (function dfs(vertex) {
+      if (!vertex) return null;
+
+      result.push(vertex);
+      visited[vertex] = true;
+
+      adjacencyList[vertex].forEach((neighbour) => {
+        if (!visited[neighbour]) dfs(neighbour);
+      });
+    })(start);
+
+    return result;
+  }
 }
 
 const g = new Graph();
+/*
 g.addVertex("Amritsar");
 g.addVertex("Delhi");
 g.addVertex("LKO");
@@ -43,3 +64,20 @@ g.addEdge("Amritsar", "LKO");
 // g.removeEdge("Amritsar", "LKO")
 g.removeVertex("Delhi");
 console.log(g);
+*/
+
+g.addVertex("A");
+g.addVertex("B");
+g.addVertex("C");
+g.addVertex("D");
+g.addVertex("E");
+g.addVertex("F");
+
+g.addEdge("A", "B");
+g.addEdge("A", "C");
+g.addEdge("B", "D");
+g.addEdge("C", "E");
+g.addEdge("D", "E");
+g.addEdge("D", "F");
+g.addEdge("E", "F");
+g.depthFirstRecursive("A");
