f0c3d93695fa74c5f2c5ab02aa0287fbe329e3ed
Sanchit Bhalla <sanchitbhalla91@gmail.com>
2024-12-16 21:02:05 +0530

    Weighted graph

---
+++ Graph/weightedGraph.js
@@ -0,0 +1,54 @@
+// Simple but bad Priority Queue implementation bcz its complexity is high
+class PriorityQueue {
+  constructor() {
+    this.values = [];
+  }
+
+  // O(N * log N)
+  enqueue(val, priority) {
+    this.values.push({ val, priority });
+    this.sort();
+  }
+
+  dequeue() {
+    return this.values.shift();
+  }
+
+  sort() {
+    this.values.sort((a, b) => a.priority - b.priority);
+  }
+}
+
+class WeightedGraph {
+  constructor() {
+    this.adjacencyList = [];
+  }
+
+  addVertex(vertex) {
+    if (!this.adjacencyList[vertex]) this.adjacencyList[vertex] = [];
+  }
+
+  addEdge(v1, v2, weight) {
+    this.adjacencyList[v1].push({ node: v2, weight });
+    this.adjacencyList[v2].push({ node: v1, weight });
+  }
+}
+
+const wg = new WeightedGraph();
+wg.addVertex("A");
+wg.addVertex("B");
+wg.addVertex("C");
+wg.addVertex("D");
+wg.addVertex("E");
+wg.addVertex("F");
+
+wg.addEdge("A", "B", 4);
+wg.addEdge("A", "C", 2);
+wg.addEdge("B", "E", 3);
+wg.addEdge("C", "D", 2);
+wg.addEdge("C", "F", 4);
+wg.addEdge("D", "E", 3);
+wg.addEdge("D", "F", 1);
+wg.addEdge("E", "F", 1);
+
+console.log(wg.Dijkstra("A", "E"));
